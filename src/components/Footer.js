7dad8697840db4455315a7d241237a00695b3f95
BenedictBenjamint <siwoo98511@gmail.com>
2023-10-24 16:02:08 +0900

    feal: header 영역 기초

--- src/components/Footer.js
+++
@@ -1,5 +0,0 @@
-export default function Footer
-// javascript 코드자리
-return(
-    <footer class="footer">하단</footer>
-)
--- src/components/Header.js
+++
@@ -1,5 +0,0 @@
-export default function Header
-// javascript 코드자리
-return(
-    <header class="header">상단</header>
-)
