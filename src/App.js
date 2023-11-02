e6dd9210254ba823b76eb2a1d810e4ee9bc3dbb3
BenedictBenjamint <siwoo98511@gmail.com>
2023-11-02 17:24:06 +0900

    feat: client 최적화

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import Header from "./components/Header";
+import Recommend from "./components/Recommend";
+import Visual from "./components/Visual";
 
 function App() {
   //안녕하세요  js 자리입니다
   /*여러 줄설명하시오 */
   return (
-        <div className="wrap">
-          {/* <!--상단 영역--> */}
-          <Header />
-          {/* <!--메인 영역--> */}
-          <div className="main">
-           
-          </div>
-          {/* <!--하단 영역--> */}
-          <footer className="footer"></footer>
-        </div>
+    <div className="wrap">
+      {/* <!--상단 영역--> */}
+      <Header />
+      {/* <!--메인 영역--> */}
+      <div className="main">
+        <Visual />
+        <Recommend />
+      </div>
+      {/* <!--하단 영역--> */}
+      <footer className="footer"></footer>
+    </div>
   );
 }
 
